b0411adb0b9fac68928128ca6abecb91d00148f2
Aiebobo <guethomas3@gmail.com>
2016-01-10 18:18:05 +0100

    Synchro

--- app/src/main/java/com/vlstr/valentin/lecampusambiant/MainActivity.java
+++ app/src/main/java/com/vlstr/valentin/lecampusambiant/MainActivity.java
@@ -1,16 +1,37 @@
 package com.vlstr.valentin.lecampusambiant;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    boolean favSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.action_bar_container, new PlaceholderFragment("vueResto"))
+                    .addToBackStack("vueResto")
+                    .commit();
+        }*/
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
@@ -32,6 +53,79 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        if (id == android.R.id.home) {
+            finish();
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+    public void onBtnClicked(View v) {
+        if (v.getId() == R.id.fav) {
+            if (!favSet) {
+                ImageButton img;
+                img = (ImageButton) this.findViewById(R.id.fav);
+                img.setImageResource(R.mipmap.ic_fav);
+                favSet = true;
+                launchToast();
+            } else {
+                ImageButton img;
+                img = (ImageButton) this.findViewById(R.id.fav);
+                img.setImageResource(R.mipmap.ic_fav_unset);
+                favSet = false;
+                launchToast();
+            }
+        } else if (v.getId() == R.id.cash) {
+
+            Intent intent = new Intent(MainActivity.this, Vue_Solde.class);
+            startActivity(intent);
+
+        } else if (v.getId() == R.id.map) {
+
+        }
+    }
+
+    public void launchToast() {
+        if (favSet) {
+            Toast.makeText(getApplicationContext(), "Added to favourites",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Removed from favourites",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    @SuppressLint("ValidFragment")
+    public static class PlaceholderFragment extends Fragment {
+
+        String value;
+
+        public PlaceholderFragment(String aValue) {
+            value = aValue;
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = null;
+            if (value.equals("vueResto")) {
+                rootView = inflater.inflate(R.layout.fragment_vue__resto, container, false);
+            } else if (value.equals("")) {
+            }
+
+
+            return rootView;
+        }
+    }
 }
+
+
--- app/src/main/java/com/vlstr/valentin/lecampusambiant/Vue_Liste_Map.java
+++ app/src/main/java/com/vlstr/valentin/lecampusambiant/Vue_Liste_Map.java
@@ -1,76 +1,163 @@
 package com.vlstr.valentin.lecampusambiant;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class Vue_Liste_Map extends FragmentActivity implements OnMapReadyCallback{
+public class Vue_Liste_Map extends AppCompatActivity implements OnMapReadyCallback {
 
-    private MapFragment mMapFragment;
     ListView listeRestaurants;
+    private MapFragment mMapFragment;
+    View.OnClickListener handlerDrag = new View.OnClickListener() {
+        public void onClick(View v) {
+            if (mMapFragment.getView().getVisibility() == View.VISIBLE) {
+                mMapFragment.getView().setVisibility(View.INVISIBLE);
+                LinearLayout.LayoutParams paramsMap = (LinearLayout.LayoutParams) mMapFragment.getView().getLayoutParams();
+                LinearLayout.LayoutParams paramsList = (LinearLayout.LayoutParams) listeRestaurants.getLayoutParams();
+                paramsList.weight = 95;
+                paramsMap.weight = 0;
+                mMapFragment.getView().setLayoutParams(paramsMap);
+                listeRestaurants.setLayoutParams(paramsList);
+            } else {
+                mMapFragment.getView().setVisibility(View.VISIBLE);
+                LinearLayout.LayoutParams paramsMap = (LinearLayout.LayoutParams) mMapFragment.getView().getLayoutParams();
+                LinearLayout.LayoutParams paramsList = (LinearLayout.LayoutParams) listeRestaurants.getLayoutParams();
+                paramsList.weight = 45;
+                paramsMap.weight = 40;
+                mMapFragment.getView().setLayoutParams(paramsMap);
+            }
+        }
+    };
     private GoogleMap googleMap = null;
-    private ArrayList<Marker> list_marker= null;
-    private String[] titles;
-    private Button dragButton;
+    //Slider
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vue_liste_map);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         listeRestaurants = (ListView) findViewById(R.id.list);
-        dragButton = (Button) findViewById(R.id.dragButton);
+        Button dragButton = (Button) findViewById(R.id.dragButton);
+
+        //sliding
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        addDrawerItems();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.title_activity_map__fragment,  /* "open drawer" description */
+                R.string.drawer_open  /* "close drawer" description */
+        ) {
+            /**
+             * Called when a drawer has settled in a completely closed state.
+             */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(R.string.title_activity_map__fragment);
+            }
+
+            /**
+             * Called when a drawer has settled in a completely open state.
+             */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.drawer_open);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
         dragButton.setOnClickListener(handlerDrag);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    View.OnClickListener handlerDrag = new View.OnClickListener() {
-        public void onClick(View v) {
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
 
-            if (mMapFragment.getView().getVisibility()==View.VISIBLE) {
-                mMapFragment.getView().setVisibility(View.INVISIBLE);
-                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)mMapFragment.getView().getLayoutParams();
-                params.weight=0;
-                mMapFragment.getView().setLayoutParams(params);
-            }else {
-                mMapFragment.getView().setVisibility(View.VISIBLE);
-                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)mMapFragment.getView().getLayoutParams();
-                params.weight=40;
-                mMapFragment.getView().setLayoutParams(params);
-            }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
-    };
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = {"Mes abonnements", "Me connecter", "Paramètres", "Aide"};
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Vue_Liste_Map.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_liste, menu);
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -78,12 +165,12 @@ public class Vue_Liste_Map extends FragmentActivity implements OnMapReadyCallbac
         googleMap.getUiSettings().setZoomControlsEnabled(true);
         googleMap.setMyLocationEnabled(true);
         LatLng insa = new LatLng(45.781936, 4.872686);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(insa,15));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(insa, 15));
         initMapExample();
     }
 
-    private void initMapExample(){
-        list_marker = new ArrayList<Marker>();
+    private void initMapExample() {
+        ArrayList<Marker> list_marker = new ArrayList<Marker>();
         list_marker.add(googleMap.addMarker(new MarkerOptions()
                 .position(new LatLng(45.781094, 4.873474))
                 .title("Castor&Pollux")));
@@ -93,15 +180,51 @@ public class Vue_Liste_Map extends FragmentActivity implements OnMapReadyCallbac
         list_marker.add(googleMap.addMarker(new MarkerOptions()
                 .position(new LatLng(45.784154, 4.874746))
                 .title("L'Olivier")));
-        titles = new String[list_marker.size()];
-        for (int i = 0; i < list_marker.size(); i++){
+        list_marker.add(googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(45.777173, 4.874529))
+                .title("Tacos Snoop Dogg")));
+        list_marker.add(googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(45.779932, 4.877215))
+                .title("Toï Toï Le Zinc")));
+        list_marker.add(googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(45.778924, 4.872799))
+                .title("Ninkasi La Doua")));
+        list_marker.add(googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(45.778672, 4.873654))
+                .title("Mosaïc")));
+        String[] titles = new String[list_marker.size()];
+        for (int i = 0; i < list_marker.size(); i++) {
             titles[i] = list_marker.get(i).getTitle();
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,android.R.id.text1,titles);
+                android.R.layout.simple_list_item_1, android.R.id.text1, titles);
 
         listeRestaurants.setAdapter(adapter);
         listeRestaurants.setClickable(true);
+        //listener de zoom sur list
+        /*listeRestaurants.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0,
+                                    View view, int position, long id) {
+                String item = (String) arg0.getItemAtPosition(position);
+                for (int i = 0; i < list_marker.size(); i++){
+                    Marker selected = list_marker.get(i);
+                    if (selected.getTitle().equals(item)) {
+                        mMapFragment.getMap().animateCamera(CameraUpdateFactory.newLatLngZoom(selected.getPosition(),17), 1000, null);
+                        selected.showInfoWindow();
+                        break;
+                    }
+                }
+            }
+        });;*/
+        listeRestaurants.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0,
+                                    View view, int position, long id) {
+                Intent intent = new Intent(Vue_Liste_Map.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
